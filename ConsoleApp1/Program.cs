9f65c3219c77fb4831e963142978528a8b2fd540
Zit87 <akotenkov899@gmail.com>
2026-09-02 15:27:46 +0300

    так доделаю

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,15 +18,123 @@ namespace ConsoleApp1
             decimal imt = ves / (rost / 100 * rost / 100);
             Console.Write("ваш ИМТ:(округленное)");
             Console.WriteLine(Convert.ToInt64(imt));
+            
 
+
+        }
+
+        static void den()
+        {
+            Console.Write("введите день:");
+            string den = Console.ReadLine();
+            Console.Write("введите месяц:");
+            int mes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите год:");
+            string god = Console.ReadLine();
+            int den1 = Convert.ToInt32(den);
+            bool god1 = Convert.ToInt32(god) % 4==0;
+
+            switch (mes)
+            {
+                case 1:
+                    if (den1  <= 31)
+                    {
+                        Console.Write(den);
+                    }else
+                    {
+                        Console.Write("стоко дней нет");
+                        break;
+                    }
+                    Console.Write(" январь ");
+                    Console.Write("20"+god);
+
+                    break;
+                case 2:
+                   if (god1 = true)
+                    {
+                        if(den1 <= 29)
+                        {
+                            Console.Write(den);
+                        }
+                    }
+                    else
+                    {
+                        if (den1 <= 28)
+                        {
+                            Console.Write(den);
+                        }
+                    }
+                    Console.Write(" февраль ");
+                    Console.Write("20" + god);
+                    break;
+                case 3:
+                    Console.Write(den);
+                    Console.Write(" март ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 4:
+                    Console.Write(den);
+                    Console.Write(" апрель ");
+                    Console.Write("20" + god);
+                    break;
+                case 5:
+                    Console.Write(den);
+                    Console.Write(" май ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 6:
+                    Console.Write(den);
+                    Console.Write(" июнь ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 7:
+                    Console.Write(den);
+                    Console.Write(" июль ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 8:
+                    Console.Write(den);
+                    Console.Write(" август ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 9:
+                    Console.Write(den);
+                    Console.Write(" сентябрь ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 10:
+                    Console.Write(den);
+                    Console.Write(" октябрь ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 11:
+                    Console.Write(den);
+                    Console.Write(" ноябрь ");
+                    Console.Write("20" + god);
+
+                    break;
+                case 12:
+                    Console.Write(den);
+                    Console.Write(" декабрь ");
+                    Console.Write("20" + god);
+
+                    break;
+            }
 
 
         }
 
         static void Main(string[] args)
         {
-            imt();
-
+            //imt();
+            den();
 
 
 
