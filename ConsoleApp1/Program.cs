f4227a420dede87b398f1fd295e9bc19a3a31587
Zit87 <akotenkov899@gmail.com>
2026-09-17 08:59:55 +0300

    додеп

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,173 +9,91 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp1
 {
-    class Lampa
+    class Order
     {
-        public static int ID = -1;
-        public string sost { get; private set; }
+        private static int nextID = 0;
+        public int ID;
+        public string Name { get; set; }
+        private double price;
 
-        public int yar;
-
-        public int temp;
-
-        public string regim { get; private set; }
-
-        public Lampa(string sost, int yar, int temp)
+        public double Price
         {
-            ID++;
-            if (sost == "on" || sost == "off")
+            get
             {
-                this.sost = sost;
-
+                return price;
             }
-            else
+            set
             {
-                Environment.Exit(1);
+                if (value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Цена должна быть больше 0");
+                }
             }
-
-            if (yar < 0 || yar > 100)
-            {
-                Console.WriteLine("от 0 до 100 только!!!");
-                Environment.Exit(1);
-
-            }
-            else
-            {
-                this.yar = yar;
-            }
-            if (temp < 2700 || temp > 6500)
-            {
-                Console.WriteLine("от 2700 до 6500 только!!!");
-                Environment.Exit(1);
-
-            }
-            else
-            {
-                this.temp = temp;
-            }
-
-            if (temp <= 3000)
-            {
-                regim = "Ночь";
-                
-            }
-            else if (temp > 3000 || temp <= 5000)
-            {
-                regim = "Чтение";
-            }
-            else if (temp > 5000)
-            {
-                regim = "День";
-            }
-
-
-
-
         }
+        private double discount;
+
+        public double Discount
+        {
+            get
+            {
+                return discount;
+            }
+            set
+            {
+                if (value >= 0 && value <= 50)
+                {
+                    discount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Скидка должна быть от 0 до 50");
+                }
+            }
+        }
+        private int kolvo;
+        public int Kolvo
+        {
+            get
+            {
+                return kolvo;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    kolvo = value;
+                }
+                else
+                {
+                    Console.WriteLine("Количество должно быть больше 0");
+                }
+            }
+        }
+        public double TotalPrice    
+        {
+            get
+            {
+                return Price * Kolvo * (1 - Discount / 100);
+            }
+        }
+
+        public Order(string Name, double Price, double Discount, int Kolvo)
+        {
+            ID = ++nextID;
+            this.Name = Name;
+            this.Price = Price;
+            this.Discount = Discount;
+            this.Kolvo = Kolvo;
+        }
+
+
         public void PrintInfo()
         {
-            Console.WriteLine($"ID лампы: {ID} состояние: {sost} яркость: {yar} температура: {temp} режим: {regim}");
-        }
-
-
-        public void onoff()
-        {
-            if(sost == "on")
-            {
-                sost = "off";
-            }
-            else
-            {
-                sost = "on";
-            }
-        }
-        public void yarkost()
-        {
-            Console.WriteLine("Введите желаемую яркость:");
-            int b = Convert.ToInt32(Console.ReadLine());
-            if (b < 0 || b > 100)
-            {
-                Console.WriteLine("от 0 до 100 только!!!");
-                return;
-
-            }
-            else
-            {
-                yar = b;
-            }
-        }
-
-        public void tempa()
-        {
-            Console.WriteLine("Введите желаемую температуру:");
-            int с = Convert.ToInt32(Console.ReadLine());
-            if (с < 2700 || с > 6500)
-            {
-                Console.WriteLine("от 2700 до 6500 только!!!");
-                return;
-
-            }
-            else
-            {
-               temp = с;
-            }
-
-            if (temp <= 3000)
-            {
-                regim = "Ночь";
-
-            }
-            else if (temp > 3000 && temp <= 5000)
-            {
-                regim = "Чтение";
-            }
-            else if (temp > 5000)
-            {
-                regim = "День";
-            }
-
-
-        }
-
-        public void regimi()
-        {
-            Console.WriteLine("Введите желаемый режим:");
-            string b = Console.ReadLine();
-            string d = b.ToLower();
-            if (d == "ночь")
-            {
-                temp = 2000;
-
-            } else if(d == "чтение")
-            {
-                temp = 4000;
-            }
-            else if (d == "день")
-            {
-                temp = 6000;
-            }
-            else
-            {
-                Console.WriteLine("Такого режима нет!!!");
-                return;
-            }
-            if (temp <= 3000)
-            {
-                regim = "Ночь";
-
-            }
-            else if (temp > 3000 && temp <= 5000)
-            {
-                regim = "Чтение";
-            }
-            else if (temp > 5000)
-            {
-                regim = "День";
-            }
-
-
-
-
+            Console.WriteLine($"ID: {ID} Name: {Name} Price: {Price} Discount: {Discount} Kolvo: {Kolvo} TotalPrice: {TotalPrice}");
         }
 
 
@@ -190,42 +108,48 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Lampa lampa = new Lampa("off", 20, 2800);
-            
-            while (true)
+            List<Order> orders = new List<Order>();
+            orders.Add(new Order("Товар 1", 100, 10, 2));
+            orders.Add(new Order("Товар 2", 200, 20, 3));
+            orders.Add(new Order("Товар 3", 300, 30, 4));
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("0 - Вывод");
+            Console.WriteLine("1 - Добавить заказ");
+            Console.WriteLine("2 - Удалить заказ");
+            Console.WriteLine("3 - Изменить заказ");
+            Console.WriteLine("4 - Вывести все заказы");
+            Console.WriteLine("5 - Вывести заказы с ценой больше заданной");
+            Console.WriteLine("6 - Вывести заказы с ценой меньше заданной");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
             {
-                
-                lampa.PrintInfo();
-                Console.WriteLine("1. Вкл/Выкл");
-                Console.WriteLine("2. Изменить яркость");
-                Console.WriteLine("3. Изменить температуру");
-                Console.WriteLine("4. Изменить режим");
-                Console.WriteLine("0. Выход");
-                int a = Convert.ToInt32(Console.ReadLine());
-                switch (a)
-                {
-                    case 1:
-                        lampa.onoff();
-                        break;
+                case 0:
+                    foreach(Order d in orders)
+                    {
+                        d.PrintInfo();
+
+                    }
+                    break;
+                case 1:
+                    Console.WriteLine("Введите название товара: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Введите цену товара: ");
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите скидку на товар: ");
+                    double discount = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите количество товара: ");
+                    int kolvo = Convert.ToInt32(Console.ReadLine());
+                    orders.Add(new Order(name, price, discount, kolvo));
+                    break;
                     case 2:
-                        lampa.yarkost();
-                            break;
-                    case 3:
-                        lampa.tempa();
-                        break;
-                    case 4:
-                        lampa.regimi();
-                        break;
-                    case 0:
-                        return;
-                    default:
-                        return;
-                }
+                    Console.WriteLine("Введите ID заказа, который хотите удалить: ");
+                    int idToRemove = Convert.ToInt32(Console.ReadLine());
+                    Order orderToRemove = orders.FirstOrDefault(o => o.ID == idToRemove);
+                    Console.WriteLine("Удалено");
 
-                
-
-
+                    break;
             }
+
         }
     }
     
