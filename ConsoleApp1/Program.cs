b49ac6a48df3244d2ee32145955e0116a6afb88f
Zit87 <akotenkov899@gmail.com>
2026-09-23 13:58:55 +0300

    jopa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,88 +9,119 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp1
 {
-    class Transport {
-        public int mosch;
-
-        public int kolkol;
-        public int maxsp;
-
-        public Transport(int mosch, int kolkol, int maxsp)
-        {
-            this.kolkol = kolkol;
-            this.mosch = mosch;
-            this.maxsp = maxsp;
-        }
-    
-        public virtual void Print()
-        {
-            Console.WriteLine($"Мощность двигателя {mosch} количество колес {kolkol} макс скорость {maxsp}");
-        }
-
-    }
-    class Mashina : Transport{
-        public string brend;
-        public Mashina(int mosch, int kolkol, int maxsp, string brend) : base(mosch, kolkol, maxsp)
-        {
-            this.brend = brend;
-        }
-
-        public override void Print()
-        {
-            base.Print();
-            Console.WriteLine($"Бренд {brend} ");
-        }
-
-    }
-    class Skuter : Transport {
-        public string tipdv;
-        public Skuter(int mosch, int kolkol, int maxsp, string tipdv) : base(mosch, kolkol, maxsp)
-        {
-            this.tipdv = tipdv;
-        }
-
-        public override void Print()
-        {
-            base.Print();
-            Console.WriteLine($"Тип двигателя {tipdv} ");
-        }
-    }
-    class Elektrosamokat : Transport {
-        public int oba;
-        public Elektrosamokat(int mosch, int kolkol, int maxsp, int oba) : base(mosch, kolkol, maxsp)
-        {
-            this.oba = oba;
-        }
-
-        public override void Print()
-        {
-            base.Print();
-            Console.WriteLine($"Обьем аккамулятора  {oba} ");
-        }
-    }
-
- 
 
 
     internal class Program
     {
 
+        class Person
+        {
+            
+            public string jopa;
 
+            public Person(string jopa) { 
+            this.jopa = jopa;
+
+            }
+            virtual public void PrintInfo()
+            {
+                
+            }
+        }
+
+        class Student : Person
+        {
+            public int Id { get; set; }
+            public string jopa2;
+
+            public Student(int Id, string jopa2, string jopa) : base (jopa)
+            {
+                this.Id = Id;
+                this.jopa2 = jopa2;
+            }
+            override public void PrintInfo()
+            {
+                Console.WriteLine($"{Id}  {jopa2} {jopa}");
+            }
+        }
+
+        class Teacher : Person
+        {
+            public string jopa3;
+            public int mani;
+
+            public Teacher(int mani, string jopa3, string jopa) : base(jopa)
+            {
+                this.jopa3 = jopa3;
+                this.mani = mani;
+            }
+            override public void PrintInfo()
+            {
+                Console.WriteLine($"{jopa3}  {mani} {jopa}");
+            }
+        }
+
+
+        class Guest : Person
+        {
+            public int skebob;
+            public string jopa4;
+
+
+            public Guest(int skebob, string jopa4, string jopa) : base(jopa)
+            {
+                this.jopa4 = jopa4;
+                this.skebob = skebob;
+            }
+
+            override public void PrintInfo()
+            {
+                Console.WriteLine($"{skebob}  {jopa4} {jopa}");
+            }
+        }
 
         static void Main(string[] args)
         {
-            var transportall = new List<Transport>{
-                new Mashina(1500, 4 , 320, "mesedes"),
-                new Skuter(50,2,60,"2такта"),
-                new Elektrosamokat(250,2,55,50000),
-            };
+            List<Person> person = new List<Person>();
+            
 
-            foreach (var t in transportall)
+
+
+            person.Add(new Student(1,"жопа2", "жопа"));
+            person.Add(new Teacher(67,"жопа3", "жопа"));
+            person.Add(new Guest(52,"жопа4", "жопа"));
+
+
+            foreach (Person p in person)
             {
-                t.Print();
-            }
 
+                if (p is Student stud)
+                {
+                    stud.PrintInfo();
+                    continue;
+                }
+               else if (p is Teacher tech)
+                {
+                    tech.PrintInfo();
+                    continue;
+                }else if (p is Guest gus)
+                {
+                    gus.PrintInfo();
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Преобразование не допустимо");
+                }
+
+
+
+
+
+
+
+            }
         }
+
     }
-    
 }
